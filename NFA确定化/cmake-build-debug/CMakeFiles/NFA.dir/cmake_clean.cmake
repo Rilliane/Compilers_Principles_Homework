9af9164dbacfe954b85fc3649249ca5d55b4bc0f
Rilliane <1723072376@qq.com>
2020-04-30 19:34:24 +0800

    Create cmake_clean.cmake

---
+++ NFA确定化/cmake-build-debug/CMakeFiles/NFA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NFA.dir/main.cpp.o"
+  "NFA.pdb"
+  "NFA"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NFA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
